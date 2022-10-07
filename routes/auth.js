6ab7361b13e79ce7e4581fcbbf448ce4d39a4cf1
Hilario <hbarrenkua@gmail.com>
2022-10-07 11:57:41 +0200

    Google SignIn

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const {Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 
@@ -13,6 +13,15 @@ router.post('/login',[
 
 ],
 login );
+
+
+router.post('/google',[
+    check('id_token','id_token es necesario').not().isEmpty(),
+    validarCampos
+
+],
+googleSignIn );
+
 
 
 module.exports = router;
